65845b43f68f45e1a7ce3116e5a46e6cdc726ec5
Dinesh Majrekar <dinesh.majrekar@serverchoice.com>
2018-03-23 13:49:12 +0000

    Add followers get / set to an organisation

--- lib/pipedrive/organization.rb
+++ lib/pipedrive/organization.rb
@@ -1,12 +1,20 @@
 module Pipedrive
   class Organization < Base
-
     def persons
       Person.all(get "#{resource_path}/#{id}/persons")
     end
 
     def deals
       Deal.all(get "#{resource_path}/#{id}/deals")
+    end
+
+    def add_follower(opts = {})
+      res = post "#{resource_path}/#{id}/followers", :body => opts
+      res.success? ? true : bad_response(res,opts)
+    end
+
+    def followers
+      User.all(get "#{resource_path}/#{id}/followers")
     end
 
     class << self
